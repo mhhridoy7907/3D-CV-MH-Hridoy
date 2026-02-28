0be547f247e514d38f638dc4f4c4892315f0b2ef
MH2 HRIDOY <mhhridoy7907@gmail.com>
2026-02-28 23:38:55 +0600

    Create funtion.js

---
+++ funtion.js
@@ -0,0 +1,9 @@
+// Auto left-right rotation
+const cv = document.getElementById('cv');
+let angle = 0;
+function autoRotate() {
+    angle += 0.2;
+    cv.style.transform = `rotateY(${angle}deg) rotateX(5deg)`; // slight tilt
+    requestAnimationFrame(autoRotate);
+}
+autoRotate();
